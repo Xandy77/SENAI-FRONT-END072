d176f1d3404e6ed23115fcc870e26455df8bee74
Xandy77 <112134228+Xandy77@users.noreply.github.com>
2024-09-24 12:00:17 -0300

    E-Books
    O código cria um cabeçalho com uma imagem de fundo e uma mensagem centralizada. Falta concluir.

---
+++ Projeto.ebooks/script/script.js
@@ -0,0 +1,28 @@
+document.getElementById('homeBtn').addEventListener('click', function() {
+    alert('Você clicou em Home!');
+});
+
+document.getElementById('bibliotecaBtn').addEventListener('click', function() {
+    alert('Você clicou em Biblioteca Digital!');
+});
+
+document.getElementById('blogBtn').addEventListener('click', function() {
+    alert('Você clicou em Blog!');
+});
+
+document.getElementById('sobreNosBtn').addEventListener('click', function() {
+    alert('Você clicou em Sobre Nós!');
+});
+
+// Novos botões de redes sociais
+document.getElementById('facebookBtn').addEventListener('click', function() {
+    alert('Você clicou em Facebook!');
+});
+
+document.getElementById('twitterBtn').addEventListener('click', function() {
+    alert('Você clicou em Twitter!');
+});
+
+document.getElementById('instagramBtn').addEventListener('click', function() {
+    alert('Você clicou em Instagram!');
+});
