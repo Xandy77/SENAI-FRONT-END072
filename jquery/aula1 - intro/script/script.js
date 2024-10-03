8ec019c496bad4b6d0c61a621ba99da31b922437
xandy77 <domirops@yahoo.com.br>
2024-10-03 11:55:00 -0300

    jQuery
    Este código usa jQuery para mudar o conteúdo e a aparência de uma caixa na página quando você clica em diferentes botões.

---
+++ jquery/aula1 - intro/script/script.js
@@ -0,0 +1,39 @@
+console.log($('#caixa'))
+
+$('#caixa').text(undefined)
+$('#caixa').html('<h1>MANEIRO</h1>')
+
+$('#teste1').on('click', function(){
+    alert('FUNFO');
+
+})
+$('#teste2').on('click', function(){
+    $('#caixa').css('width', '300px')
+    $('#caixa').css('backgroundColor', 'red')
+})
+
+$('#teste3').on('click', function(){
+
+    if($('#caixa').hasClass('primeiroEstado')){
+        $('#caixa').addClass('segundoEstado')
+        $('#caixa').removeClass('primeiroEstado')
+    }else{
+        $('#caixa').toggleClass('segundoEstado','primeiroEstado')
+    }
+})
+
+$('#teste4').on('click', function(){
+        // $('#caixa').slideToggle(500)
+        // $('#caixa').hide(500)
+        $('#caixa').fadeOut(500)
+
+
+})
+
+$('#teste5').on('click', function(){
+    // $('#caixa').slideToggle(500)
+    // $('#caixa').hide(500)
+    $('#caixa').fadeIn(500)
+
+
+})
